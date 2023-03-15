6604827a217359d71a2450833bb9ab0ae1be97de
4rqan <123300512+4rqan@users.noreply.github.com>
2023-03-15 17:46:26 +0530

    Updated RentalListingsPage(Showed the Data added in a Table Format)

--- client/src/pages/Landlord/RentalListingsPage.jsx
+++ client/src/pages/Landlord/RentalListingsPage.jsx
@@ -1,14 +1,50 @@
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
 import { getListings } from "../../services/listings.service";
 
 const RentalListingsPage = () => {
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     getListings(setList);
   }, []);
 
-  return <div>RentalListingsPage</div>;
+  return (
+    <Container>
+      <h3 className="text-center">Rental Listings</h3>
+      <Table striped bordered>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Property Type</th>
+            <th>Location</th>
+            <th>Price</th>
+            <th>Bedrooms</th>
+            <th>Bathrooms</th>
+            <th>Available Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((item) => {
+            return (
+              <tr key={item}>
+                <td>{item.title}</td>
+                <td>{item.description}</td>
+                <td>{item.propertyType}</td>
+                <td>{item.location}</td>
+                <td>{item.price}</td>
+                <td>{item.bedrooms}</td>
+                <td>{item.bathrooms}</td>
+                <td>{item.availableDate}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
+
 export default RentalListingsPage;
